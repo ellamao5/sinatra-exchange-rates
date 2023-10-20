21b30732f7ec8f3921b986af74ba93a5c2e50310
ellamao5 <146144905+ellamao5@users.noreply.github.com>
2023-10-20 20:36:19 +0000

    start

--- app.rb
+++ app.rb
@@ -2,8 +2,25 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  raw_data = HTTP.get(api_url)
+
+  raw_data_string = raw_data.to_s
+
+  parsed_data = JSON.parse(raw_data_string)
 end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+end
+
