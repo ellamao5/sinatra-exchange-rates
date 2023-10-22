a0bc7b05b50da426ac8476fe5375a30f7d21ae36
ellamao5 <146144905+ellamao5@users.noreply.github.com>
2023-10-22 18:56:04 +0000

    Almost

--- app.rb
+++ app.rb
@@ -12,18 +12,34 @@ get("/") do
 
   parsed_data = JSON.parse(raw_data_string)
 
-  @symbols = parsed_data.fetch("currencies")
+  symbols = parsed_data.fetch("currencies")
 
-  @abbreviations = @symbols.keys
+  @abbreviations = symbols.keys
 
 
   erb(:homepage)
 end
 
 get("/:from_currency") do
+  
   @original_currency = params.fetch("from_currency")
 
+
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  raw_data = HTTP.get(api_url)
+
+  raw_data_string = raw_data.to_s
+
+  parsed_data = JSON.parse(raw_data_string)
+
+  symbols = parsed_data.fetch("currencies")
+
+  @abbreviations = symbols.keys
+  
+
+  erb(:currency)
+
 end
 
 get("/:from_currency/:to_currency") do
@@ -31,5 +47,7 @@ get("/:from_currency/:to_currency") do
   @destination_currency = params.fetch("to_currency")
 
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+
+  erb(:currency_conversion)
 
 end
